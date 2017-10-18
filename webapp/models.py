90a50959ec0da692e1723f9e86e1a839cdb54c39
j-silverman <jhs2018@bu.edu>
2017-10-18 17:13:44 -0400

    added People table

--- webapp/models.py
+++ webapp/models.py
@@ -3,4 +3,12 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+class people(models.Model):
+    first_name = models.CharField(max_length = 20)
+    last_name = models.CharField(max_length = 20)
+    email = models.CharField(max_length = 30)
+    home_town = models.CharField(max_length= 20)
+    gender = models.CharField(max_length=1)
+    password = models.CharField(max_length = 20)
+    date_of_birth = models.DateField()
+    
